1385972ed1a8c8e67a9feab332c19f10b5f3b2a2
Guangyi Shang <guangyi.shang@gmail.com>
2019-02-02 16:15:31 -0800

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by 尚光益 on 2/2/19.
+//  Copyright © 2019 Guangyi Shang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
